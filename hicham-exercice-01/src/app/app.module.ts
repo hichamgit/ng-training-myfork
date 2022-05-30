990ff8efe72b640e34f2c8c92c893751ab0ce840
Hicham <hicham.ta@gmail.com>
2022-05-30 14:47:29 +0100

    Adding notification-button component

---
+++ hicham-exercice-01/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NotificationButtonComponent } from './notification-button/notification-button.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NotificationButtonComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
